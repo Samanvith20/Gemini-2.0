4b4062b4187de5e62ba6a87867097d0bfedcb53a
Samanvith <Samanvith2005@gmail.com>
2024-05-29 16:03:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./componenrs/main/Main"
+import Sidebar from "./componenrs/sidebar/Sidebar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componenrs/main/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+  return (
+    <div>
+      Main component
+    </div>
+  )
+}
+
+export default Main
